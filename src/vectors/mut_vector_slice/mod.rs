f1e64e3a9849ae5cb3a79090ffb94c2c80c94b02
Eric Ovenden <eric2ovenden@gmail.com>
2023-05-08 02:59:41 -0500

    implemented 'From' for MutVectorSlice

--- src/vectors/mut_vector_slice/mod.rs
+++ src/vectors/mut_vector_slice/mod.rs
@@ -1,4 +1,4 @@
-use super::MutVectorSlice;
+use super::{MutVectorSlice, MutVector};
 
 mod add;
 mod mul;
@@ -13,5 +13,14 @@ impl<'a, T> MutVectorSlice<'a, T> {
 
     pub fn end(&self) -> usize {
         self.slice_range.end
+    }
+}
+
+impl<'a, T> From<&'a MutVector<T>> for MutVectorSlice<'a, T> {
+    fn from(mut_vector: &'a MutVector<T>) -> Self {
+        MutVectorSlice {
+            mut_vector,
+            slice_range: 0..mut_vector.len()
+        }
     }
 }
