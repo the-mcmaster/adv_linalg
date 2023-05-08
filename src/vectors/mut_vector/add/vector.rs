8ae75f249c71aff70bbecab5d9c2b3176431c910
Eric Ovenden <eric2ovenden@gmail.com>
2023-05-08 05:08:01 -0500

    added mutable add implementations for MutVector with Vector as right-hand-side
    forgot to do this the first time around

--- src/vectors/mut_vector/add/vector.rs
+++ src/vectors/mut_vector/add/vector.rs
@@ -1,8 +1,24 @@
 use std::ops::Add;
 use crate::Vector;
 use crate::vectors::MutVector;
-use crate::macros::vector_unsliced_immut_add;
+use crate::macros::{vector_unsliced_immut_add, vector_unsliced_mut_add};
 
+// -----Mut Adds-----
+impl<'r, T> Add<Vector<T>> for &'r mut MutVector<T>
+where
+    T: Clone + Add<Output = T>
+{
+    vector_unsliced_mut_add!(Vector<T>);
+}
+
+impl<'r, T> Add<&Vector<T>> for &'r mut MutVector<T>
+where
+    T: Clone + Add<Output = T>
+{
+    vector_unsliced_mut_add!(&Vector<T>);
+}
+
+// -----Immut Adds-----
 impl<T> Add<Vector<T>> for MutVector<T>
 where
     T: Clone + Add<Output = T>
