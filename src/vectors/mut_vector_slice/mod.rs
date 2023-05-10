29a5189c1b93748e080017a1ae59124b87c1a456
Eric Ovenden <eric2ovenden@gmail.com>
2023-05-10 06:29:32 -0500

    Added constructor method for vector slice types
    'new()' method for VectorSlice and MutVectorSlice need to do
    check with the given range for the slice. For this, this method
    must return a result.
    
    The Result-less alternative forces the user to create a vector
    slice with the range being the entire original range, letting the
    slice be a mutable variable, and then using the 'slice_range'
    method to change it to the desired slice.
    
    The 'slice_range' method must return a Result anyway, and anyone
    who wants to create a slice of a vector with the entire range of
    the given vector can just unwrap the Result safely.

--- src/vectors/mut_vector_slice/mod.rs
+++ src/vectors/mut_vector_slice/mod.rs
@@ -1,3 +1,7 @@
+use std::ops::Range;
+
+use crate::error::Error;
+
 use super::{MutVectorSlice, MutVector};
 
 mod add;
@@ -7,6 +11,42 @@ mod lambda;
 mod map;
 
 impl<'a, T> MutVectorSlice<'a, T> {
+    pub fn new(mut_vector: &'a MutVector<T>, slice_range: Range<usize>) -> Result<Self, Error> {
+        if slice_range.is_empty() {
+            
+            return Err(Error::RangeUnderflow("Slice range was empty"))
+        
+        } else if mut_vector.len() >= slice_range.end {
+            
+            return Err(Error::RangeOverflow("Slice range end exceeded the length of the relative vector"));
+        
+        } else {
+            
+            return Ok(MutVectorSlice {
+                vector: mut_vector,
+                slice_range
+            });
+        
+        }
+    }
+
+    pub fn slice_range(&mut self, slice_range: Range<usize>) -> Result<(), Error> {
+        if slice_range.is_empty() {
+            
+            return Err(Error::RangeUnderflow("Slice range was empty"))
+        
+        } else if self.vector.len() >= slice_range.end {
+            
+            return Err(Error::RangeOverflow("Slice range end exceeded the length of the relative vector"));
+        
+        } else {
+            
+            self.slice_range = slice_range;
+            return Ok(())
+        
+        }
+    }
+
     pub fn len(&self) -> usize {
         self.slice_range.end - self.slice_range.start
     }
--- src/vectors/vector_slice/mod.rs
+++ src/vectors/vector_slice/mod.rs
@@ -1,5 +1,8 @@
+use std::ops::Range;
+
 use crate::Vector;
 
+use crate::error::Error;
 use crate::vectors::VectorSlice;
 
 mod add;
@@ -9,6 +12,42 @@ mod lambda;
 mod map;
 
 impl<'a, T> VectorSlice<'a, T> {
+    pub fn new(vector: &'a Vector<T>, slice_range: Range<usize>) -> Result<Self, Error> {
+        if slice_range.is_empty() {
+            
+            return Err(Error::RangeUnderflow("Slice range was empty"))
+        
+        } else if vector.len() >= slice_range.end {
+            
+            return Err(Error::RangeOverflow("Slice range end exceeded the length of the relative vector"));
+        
+        } else {
+            
+            return Ok(VectorSlice {
+                vector,
+                slice_range
+            });
+        
+        }
+    }
+
+    pub fn slice_range(&mut self, slice_range: Range<usize>) -> Result<(), Error> {
+        if slice_range.is_empty() {
+            
+            return Err(Error::RangeUnderflow("Slice range was empty"))
+        
+        } else if self.vector.len() >= slice_range.end {
+            
+            return Err(Error::RangeOverflow("Slice range end exceeded the length of the relative vector"));
+        
+        } else {
+            
+            self.slice_range = slice_range;
+            return Ok(())
+        
+        }
+    }
+
     pub fn len(&self) -> usize {
         self.slice_range.end - self.slice_range.start
     }
