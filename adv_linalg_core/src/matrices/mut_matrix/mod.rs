6a5cdf9213ca3386ff6f84dfe9450084cb99a00f
Eric Ovenden <eric2ovenden@gmail.com>
2023-05-14 03:12:33 -0500

    implementated from contructors for MutMatrix

--- adv_linalg_core/src/matrices/mut_matrix/mod.rs
+++ adv_linalg_core/src/matrices/mut_matrix/mod.rs
@@ -1,5 +1,182 @@
+use crate::Matrix;
+use crate::matrices::{MatrixBlock, MutMatrix, MutMatrixBlock};
+
 mod add;
 mod mul;
 mod sub;
 mod lambda;
 mod map;
+
+impl<T> From<Vec<Vec<T>>> for MutMatrix<T> {
+    fn from(matrix: Vec<Vec<T>>) -> Self {
+        let rows = matrix.len();
+        let cols = matrix[0].len();
+
+        for row in 1..rows {
+            if matrix[row].len() != cols {
+                panic!("Input 2D Vec does not have same length for all rows")
+            }
+        }
+        MutMatrix {
+            matrix, 
+            rows, 
+            cols
+        }
+    }
+}
+
+impl<T> From<&Vec<Vec<T>>> for MutMatrix<T>
+where
+    T: Clone
+{
+    fn from(matrix: &Vec<Vec<T>>) -> Self {
+        let rows = matrix.len();
+        let cols = matrix[0].len();
+
+        for row in 1..rows {
+            if matrix[row].len() != cols {
+                panic!("Input 2D Vec does not have same length for all rows")
+            }
+        }
+        MutMatrix {
+            matrix: matrix.clone(), 
+            rows, 
+            cols
+        }
+    }
+}
+
+impl<T> From<MatrixBlock<'_, T>> for MutMatrix<T>
+where
+    T: Clone
+{
+    fn from(matrix_block: MatrixBlock<T>) -> Self {
+        let rows = matrix_block.rows();
+        let cols = matrix_block.cols();
+
+        let mut matrix = vec![Vec::with_capacity(matrix_block.cols()); matrix_block.rows()];
+
+        for row in matrix_block.matrix.matrix
+                            .split_at(matrix_block.row_start()).1
+                            .split_at(matrix_block.row_end()).0 {
+            matrix.push(row
+                .split_at(matrix_block.col_start()).1
+                .split_at(matrix_block.col_end()).0
+                .into()
+            );
+        }
+
+        MutMatrix {
+            matrix, 
+            rows, 
+            cols
+        }
+    }
+}
+
+impl<T> From<&MatrixBlock<'_, T>> for MutMatrix<T>
+where
+    T: Clone
+{
+    fn from(matrix_block: &MatrixBlock<T>) -> Self {
+        let rows = matrix_block.rows();
+        let cols = matrix_block.cols();
+
+        let mut matrix = vec![Vec::with_capacity(matrix_block.cols()); matrix_block.rows()];
+
+        for row in matrix_block.matrix.matrix
+                            .split_at(matrix_block.row_start()).1
+                            .split_at(matrix_block.row_end()).0 {
+            matrix.push(row
+                .split_at(matrix_block.col_start()).1
+                .split_at(matrix_block.col_end()).0
+                .into()
+            );
+        }
+
+        MutMatrix {
+            matrix, 
+            rows, 
+            cols
+        }
+    }
+}
+
+impl<T> From<MutMatrixBlock<'_, T>> for MutMatrix<T>
+where
+    T: Clone
+{
+    fn from(matrix_block: MutMatrixBlock<T>) -> Self {
+        let rows = matrix_block.rows();
+        let cols = matrix_block.cols();
+
+        let mut matrix = vec![Vec::with_capacity(matrix_block.cols()); matrix_block.rows()];
+
+        for row in matrix_block.matrix.matrix
+                            .split_at(matrix_block.row_start()).1
+                            .split_at(matrix_block.row_end()).0 {
+            matrix.push(row
+                .split_at(matrix_block.col_start()).1
+                .split_at(matrix_block.col_end()).0
+                .into()
+            );
+        }
+
+        MutMatrix {
+            matrix, 
+            rows, 
+            cols
+        }
+    }
+}
+
+impl<T> From<&MutMatrixBlock<'_, T>> for MutMatrix<T>
+where
+    T: Clone
+{
+    fn from(matrix_block: &MutMatrixBlock<T>) -> Self {
+        let rows = matrix_block.rows();
+        let cols = matrix_block.cols();
+
+        let mut matrix = vec![Vec::with_capacity(matrix_block.cols()); matrix_block.rows()];
+
+        for row in matrix_block.matrix.matrix
+                            .split_at(matrix_block.row_start()).1
+                            .split_at(matrix_block.row_end()).0 {
+            matrix.push(row
+                .split_at(matrix_block.col_start()).1
+                .split_at(matrix_block.col_end()).0
+                .into()
+            );
+        }
+
+        MutMatrix {
+            matrix, 
+            rows, 
+            cols
+        }
+    }
+}
+
+impl<T> From<Matrix<T>> for MutMatrix<T> {
+    fn from(matrix: Matrix<T>) -> Self {
+        MutMatrix {
+            matrix: matrix.matrix,
+            rows: matrix.rows,
+            cols: matrix.cols
+        }
+    }
+}
+
+impl<T> From<&Matrix<T>> for MutMatrix<T>
+where
+    T: Clone
+{
+    fn from(matrix: &Matrix<T>) -> Self {
+        MutMatrix {
+            matrix: matrix.matrix.clone(),
+            rows: matrix.rows,
+            cols: matrix.cols
+        }
+    }
+}
