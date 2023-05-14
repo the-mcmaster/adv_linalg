b251c30082c76a0d5b71e804280d7b86c7e9512a
Eric Ovenden <eric2ovenden@gmail.com>
2023-05-13 22:58:49 -0500

    Added derived traits to all vector types

--- src/vectors/mod.rs
+++ src/vectors/mod.rs
@@ -5,19 +5,23 @@ mod mut_vector_slice;
 
 use std::ops::Range;
 
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Vector<T> {
     pub(crate) list : Vec<T>
 }
 
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct VectorSlice<'a, T> {
     pub(crate) vector : &'a Vector<T>,
     slice_range : Range<usize>
 }
 
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct MutVector<T> {
     pub(crate) list : Vec<T>
 }
 
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct MutVectorSlice<'a, T> {
     pub(crate) vector : &'a MutVector<T>,
     slice_range : Range<usize>
