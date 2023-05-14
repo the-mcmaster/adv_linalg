aba9c882674a8ceed236b1fcd3cbd96a47e9e884
Eric Ovenden <eric2ovenden@gmail.com>
2023-05-14 03:41:26 -0500

    implemented from contructors for MatrixBlock

--- adv_linalg_core/src/matrices/matrix_block/mod.rs
+++ adv_linalg_core/src/matrices/matrix_block/mod.rs
@@ -1,3 +1,8 @@
+use std::ops::Range;
+
+use crate::Matrix;
+use crate::error::Error;
+
 use super::MatrixBlock;
 
 mod add;
@@ -8,6 +13,60 @@ mod map;
 
 impl<'a, T> MatrixBlock<'a, T>
 {
+    pub fn new(matrix: &'a Matrix<T>, row_range: Range<usize>, col_range: Range<usize>) -> Result<Self, Error> {
+        if row_range.is_empty() {
+            return Err(Error::RangeUnderflow("Row range was empty"));
+        } else if matrix.rows >= row_range.end {
+            return Err(Error::RangeOverflow("Row range end exceeded the row length of the relative matrix"))
+        }
+        
+        if col_range.is_empty() {
+            return Err(Error::RangeUnderflow("Column range was empty"));
+        } else if matrix.cols >= col_range.end {
+            return Err(Error::RangeOverflow("Column range end exceeded the column length of the relative matrix"))
+        }
+        
+        Ok(MatrixBlock {
+            matrix,
+            row_range,
+            col_range
+        })
+    }
+
+    pub fn row_range(&mut self, row_range: Range<usize>) -> Result<(), Error> {
+        if row_range.is_empty() {
+            
+            return Err(Error::RangeUnderflow("Row range was empty"))
+        
+        } else if row_range.end >= self.matrix.rows {
+            
+            return Err(Error::RangeOverflow("Row range end exceeded the row length of the relative matrix"));
+        
+        } else {
+            
+            self.row_range = row_range;
+            return Ok(());
+        
+        }
+    }
+
+    pub fn col_range(&mut self, col_range: Range<usize>) -> Result<(), Error> {
+        if col_range.is_empty() {
+            
+            return Err(Error::RangeUnderflow("Column range was empty"))
+        
+        } else if col_range.end >= self.matrix.cols {
+            
+            return Err(Error::RangeOverflow("Column range end exceeded the column length of the relative matrix"));
+        
+        } else {
+            
+            self.col_range = col_range;
+            return Ok(());
+        
+        }
+    }
+
     pub fn rows(&self) -> usize {
         self.row_range.end - self.row_range.start
     }
@@ -30,5 +89,15 @@ impl<'a, T> MatrixBlock<'a, T>
 
     pub fn row_end(&self) -> usize {
         self.row_range.end
+    }
+}
+
+impl<'a, T> From<&'a Matrix<T>> for MatrixBlock<'a, T> {
+    fn from(matrix: &'a Matrix<T>) -> Self {
+        MatrixBlock {
+            matrix,
+            row_range: 0..matrix.rows,
+            col_range: 0..matrix.cols
+        }
     }
 }
