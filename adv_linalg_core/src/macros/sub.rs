78148b5975d6ea00651110ca85eeefe1c5969aae
Eric Ovenden <eric2ovenden@gmail.com>
2023-05-14 20:38:15 -0500

    created subtraction implementation macros for matrix types

--- adv_linalg_core/src/macros/sub.rs
+++ adv_linalg_core/src/macros/sub.rs
@@ -123,3 +123,153 @@ macro_rules! vector_unsliced_mut_sub {
     }
 }
 pub(crate) use vector_unsliced_mut_sub;
+
+macro_rules! matrix_unsliced_immut_sub {
+    ($rhs_type:ty) => {
+        type Output = Matrix<T>;
+
+        fn sub(self, rhs: $rhs_type) -> Self::Output {
+            if self.rows != rhs.rows || self.cols != rhs.cols {
+                panic!("Matrices with different sizes cannot be subtracted together.")
+            }
+
+            let mut params = Vec::with_capacity(self.rows);
+            
+            for row_idx in 0..self.rows {
+                let mut param_row = Vec::with_capacity(self.cols);
+
+                for col_idx in 0..self.cols {
+                    param_row.push(self.matrix[row_idx][col_idx].clone() - rhs.matrix[row_idx][col_idx].clone())
+                }
+                
+                params.push(param_row)
+            }
+
+            Matrix::from(params)
+        }
+    }
+}
+pub(crate) use matrix_unsliced_immut_sub;
+
+macro_rules! matrix_right_sliced_immut_sub {
+    ($rhs_type:ty) => {
+        type Output = Matrix<T>;
+
+        fn sub(self, rhs: $rhs_type) -> Self::Output {
+            if self.rows != rhs.rows() || self.cols != rhs.cols() {
+                panic!("Matrices with different sizes cannot be subtracted together.")
+            }
+            
+            let mut params = Vec::with_capacity(self.rows);
+            
+            for row_idx in 0..self.rows {
+                let mut param_row = Vec::with_capacity(self.cols);
+
+                for col_idx in 0..self.cols {
+                    param_row.push(self.matrix[row_idx][col_idx].clone() - rhs.matrix.matrix[row_idx + rhs.row_start()][col_idx + rhs.col_start()].clone())
+                }
+                
+                params.push(param_row)
+            }
+
+            Matrix::from(params)
+        }
+    }
+}
+pub(crate) use matrix_right_sliced_immut_sub;
+
+macro_rules! matrix_left_sliced_immut_sub {
+    ($rhs_type:ty) => {
+        type Output = Matrix<T>;
+
+        fn sub(self, rhs: $rhs_type) -> Self::Output {
+            if self.rows() != rhs.rows || self.cols() != rhs.cols {
+                panic!("Matrices with different sizes cannot be subtracted together.")
+            }
+            
+            let mut params = Vec::with_capacity(rhs.rows);
+            
+            for row_idx in 0..rhs.rows {
+                let mut param_row = Vec::with_capacity(rhs.cols);
+
+                for col_idx in 0..rhs.cols {
+                    param_row.push(self.matrix.matrix[row_idx + self.row_start()][col_idx + self.col_start()].clone() - rhs.matrix[row_idx][col_idx].clone())
+                }
+                
+                params.push(param_row)
+            }
+
+            Matrix::from(params)
+        }
+    }
+}
+pub(crate) use matrix_left_sliced_immut_sub;
+
+macro_rules! matrix_both_sliced_immut_sub {
+    ($rhs_type:ty) => {
+        type Output = Matrix<T>;
+
+        fn sub(self, rhs: $rhs_type) -> Self::Output {
+            if self.rows() != rhs.rows() || self.cols() != rhs.cols() {
+                panic!("Matrices with different sizes cannot be added together.")
+            }
+            
+            let mut params = Vec::with_capacity(rhs.rows());
+            
+            for row_idx in 0..rhs.rows() {
+                let mut param_row = Vec::with_capacity(rhs.cols());
+
+                for col_idx in 0..self.cols() {
+                    param_row.push(self.matrix.matrix[row_idx + self.row_start()][col_idx + self.col_start()].clone() - rhs.matrix.matrix[row_idx + rhs.row_start()][col_idx + rhs.col_start()].clone())
+                }
+                
+                params.push(param_row)
+            }
+
+            Matrix::from(params)
+        }
+    }
+}
+pub(crate) use matrix_both_sliced_immut_sub;
+
+macro_rules! matrix_sliced_mut_sub {
+    ($rhs_type:ty) => {
+        type Output = Self;
+
+        fn sub(self, rhs: $rhs_type) -> Self::Output {
+            if self.rows != rhs.rows() || self.cols != rhs.cols() {
+                panic!("Matrices with different sizes cannot be subtracted together.")
+            }
+    
+            for row_idx in 0..self.rows {
+                for col_idx in 0..self.cols {
+                    self.matrix[row_idx][col_idx] = self.matrix[row_idx][col_idx].clone() - rhs.matrix.matrix[row_idx + rhs.row_start()][col_idx + rhs.col_start()].clone()
+                }
+            }
+    
+            self
+        }
+    }
+}
+pub(crate) use matrix_sliced_mut_sub;
+
+macro_rules! matrix_unsliced_mut_sub {
+    ($rhs_type:ty) => {
+        type Output = Self;
+
+        fn sub(self, rhs: $rhs_type) -> Self::Output {
+            if self.rows != rhs.rows || self.cols != rhs.cols {
+                panic!("Matrices with different sizes cannot be subtracted together.")
+            }
+    
+            for row_idx in 0..self.rows {
+                for col_idx in 0..self.cols {
+                    self.matrix[row_idx][col_idx] = self.matrix[row_idx][col_idx].clone() - rhs.matrix[row_idx][col_idx].clone()
+                }
+            }
+    
+            self
+        }
+    }
+}
+pub(crate) use matrix_unsliced_mut_sub;
