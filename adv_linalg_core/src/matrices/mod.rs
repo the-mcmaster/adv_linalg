120b1ae8c2f338d0a2d9e6310bb58dc5403dd0f8
Eric Ovenden <eric2ovenden@gmail.com>
2023-05-17 11:05:50 -0500

    common derive macros for matrix types

--- adv_linalg_core/src/matrices/mod.rs
+++ adv_linalg_core/src/matrices/mod.rs
@@ -5,24 +5,28 @@ mod mut_matrix;
 mod matrix_block;
 mod mut_matrix_block;
 
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Matrix<T> {
     pub(crate) matrix : Vec<Vec<T>>,
     pub(crate) rows : usize,
     pub(crate) cols : usize
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct MatrixBlock<'a, T> {
     pub(crate) matrix : &'a Matrix<T>,
     row_range : Range<usize>,
     col_range : Range<usize>
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct MutMatrix<T> {
     pub(crate) matrix : Vec<Vec<T>>,
     pub(crate) rows : usize,
     pub(crate) cols : usize
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct MutMatrixBlock<'a, T> {
     pub(crate) matrix : &'a MutMatrix<T>,
     row_range : Range<usize>,
